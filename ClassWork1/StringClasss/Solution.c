6ab7a68b85fa5f1dc781e79aacbf8470999833b8
Slava Chastov <crychs142@gmail.com>
2022-10-13 10:31:51 +0300

    Реализакия задачи на строки(прототип)

--- ClassWork1/StringClasss/Solution.c
+++ ClassWork1/StringClasss/Solution.c
@@ -4,10 +4,26 @@
 int main() {
 	setlocale(LC_ALL, "rus");
 	char* str;
+	int x = 0, number, cut;
 	str = (char*)malloc(sizeof(char) * 100);
 	printf("Введите строку: ");
 	gets(str);
-	puts(str);
+	while (x != 1) {
+		printf("Выберете действия над строкой:\n1) заменить строку\n2) обрезать строку\n3) развернуть строку\n4) сменить регистр\n0) выход\n\nВаш выбор: ");
+		scanf_s("%d", &number);
+		system("cls");
+		switch (number) {
+		case 1:
+			continue;
+		case 2:
+			printf("Введите значение: ");
+			scanf_s("%d", &cut);
+			str[cut] = '\0';
+			printf("Результат: %s\n", str);
+			continue;
+		}
+		x++;
+	}
 	free(str);
 	system("pause");
 	return 0;
